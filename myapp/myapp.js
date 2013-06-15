fedd12ff75cdee87cdde240b956388deb88f555a
Tim <tim@ubuntu.(none)>
2013-06-15 15:18:17 -0700

    JS commit

--- myapp/myapp.js
+++ myapp/myapp.js
@@ -2,23 +2,15 @@ Files = new Meteor.Collection("files");
 if (Meteor.isClient) {
   Template.textinput.textstuff = function () {
     var currFile = Files.findOne({});
-    alert(currFile.text);
+    
     return  currFile.text;
   };
-
-  Template.hello.events({
-    'click input' : function () {
-      // template data, if any, is available in 'this'
-      if (typeof console !== 'undefined')
-        window.alert("PORKCHOP SANDWICHES")
-    }
-  });
 }
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
 
-        Files.insert({name: "default", text: "timmy"});
+        Files.insert({name: "default", text: "new text"});
     
   });
 }

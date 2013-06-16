00f9f51e43b59e29c3993d148c086e24fb97e4a7
Tim <tim@ubuntu.(none)>
2013-06-15 23:10:57 -0700

    concurrency stuff

--- my-app2/my-app2.js
+++ my-app2/my-app2.js
@@ -1,30 +1,37 @@
 Files = new Meteor.Collection("files");
+var editor;
 if (Meteor.isClient) {
   Meteor.startup(function () 
   {
-  	var editor=ace.edit("editor");
- 	var textarea = $('textarea[name="description"]');
-	editor.getSession().setValue(textarea.val());
-	editor.getSession().on('change', function(){textarea.val(editor.getSession().getValue());});
-	
-	var query = Files.find({_id : Session.get("files")});
+  	editor=ace.edit("editor");
+    ///
+	var query = Files.find({name : "default"});
+	//alert(Files.findOne({})._id);
 	handle = query.observe({        
-		changed : function(newDoc, oldIndex, oldDoc) 
-		{
-			if(editor !== undefined)
-			{
-				editor.setValue(newDoc.contents);
-      		}
-    	}
-	});//end handle definition
-	Template.editout.contents = function () {
-		return "lol";
-	}
+	  	changed : function(newDoc, oldDoc) {
+	      if(editor !== undefined){
+	        editor.setValue(newDoc.contents);
+	      }
+	    }
+	});
+
+  	///
+  	editor.getSession().on('change', function(e) {
+    Files.update(Files.findOne({})._id, 
+      { $set : 
+        { 
+          contents : editor.getValue()
+        }
+      })
+	});
+	///
   });//End Meteor.startup
 }//End if
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
-    
+    if (Files.find().count() === 0) {
+        Files.insert({name: "default", contents: "<Insert Content>"});
+    }    
   });
 }

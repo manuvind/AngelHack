b99e00e11d096da3b423f3901cd2c18f68b169c2
Tim <tim@ubuntu.(none)>
2013-06-15 20:41:59 -0700

    JavaScript changes

--- my-app2/my-app2.js
+++ my-app2/my-app2.js
@@ -1,14 +1,27 @@
 Files = new Meteor.Collection("files");
 if (Meteor.isClient) {
-  Meteor.startup(function () {
+  Meteor.startup(function () 
+  {
   	var editor=ace.edit("editor");
  	var textarea = $('textarea[name="description"]');
 	editor.getSession().setValue(textarea.val());
-	editor.getSession().on('change', function(){
-  	textarea.val(editor.getSession().getValue());
-});
-      });
-}
+	editor.getSession().on('change', function(){textarea.val(editor.getSession().getValue());});
+	
+	var query = Files.find({_id : Session.get("files")});
+	handle = query.observe({        
+		changed : function(newDoc, oldIndex, oldDoc) 
+		{
+			if(editor !== undefined)
+			{
+				editor.setValue(newDoc.contents);
+      		}
+    	}
+	});//end handle definition
+	Template.editout.contents = function () {
+		return "lol";
+	}
+  });//End Meteor.startup
+}//End if
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
